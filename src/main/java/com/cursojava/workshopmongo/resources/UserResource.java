21beee18bcefd08a9dbbfae4e6283f70a5bb2c7f
Matheus Oliveira <matheus.oliveirav@gmail.com>
2026-01-13 10:29:21 -0300

    update user logic

--- src/main/java/com/cursojava/workshopmongo/resources/UserResource.java
+++ src/main/java/com/cursojava/workshopmongo/resources/UserResource.java
@@ -50,4 +50,12 @@ public class UserResource {
     URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
     return ResponseEntity.created(uri).build();
   }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+  public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+    User obj = service.fromDTO(objDto);
+    obj.setId(id);
+    service.update(obj);
+    return ResponseEntity.noContent().build();
+  }
 }
