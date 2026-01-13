c3f4087fe5f1e955ca2938c44b8da38d0c8b4ef5
Matheus Oliveira <matheus.oliveirav@gmail.com>
2026-01-13 11:47:42 -0300

    get post by id

---
+++ src/main/java/com/cursojava/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.cursojava.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursojava.workshopmongo.domain.Post;
+import com.cursojava.workshopmongo.repository.PostRepository;
+import com.cursojava.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+  @Autowired
+  private PostRepository repo;
+
+  public Post findById(String id) {
+    Post post = repo.findById(id).orElse(null);
+    if(post == null){
+      throw new ObjectNotFoundException("Objeto não encontrado");
+    } else {
+      return post;
+    }
+  }
+}
